8e4fdd490494fc662fd499cba68d259952aec6fd
Tayyab-Kamboh <tayyabkambohSK28@gmail.com>
2023-07-14 22:18:17 +0200

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,12 @@
 class Comment < ApplicationRecord
-    belongs_to :post
-    belongs_to :author, class_name: 'User'
+  belongs_to :post
+  belongs_to :author, class_name: 'User'
 
-    validates :text, presence: true
+  validates :text, presence: true
 
-    after_save :update_comments_counter
+  after_save :update_comments_counter
 
-    def update_comments_counter
-      post.update(comments_counter: post.comments.count)
-    end
+  def update_comments_counter
+    post.update(comments_counter: post.comments.count)
   end
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,28 +1,28 @@
 class Post < ApplicationRecord
-    belongs_to :author, class_name: 'User', foreign_key: 'author_id'
-    has_many :likes, foreign_key: 'post_id'
-    has_many :comments, foreign_key: 'post_id'
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :likes, foreign_key: 'post_id'
+  has_many :comments, foreign_key: 'post_id'
 
-    after_save :update_post_counter
+  after_save :update_post_counter
 
-    validates :title, presence: true
-    validates :text, presence: true
-    validates :likes_counter,
-              numericality: {
-                only_integer: true,
-                greater_than_or_equal_to: 0,
-                message: 'must be an integer greater than or equal to zero'
-              },
-              on: :create
-    validates :comments_counter,
-              numericality: {
-                only_integer: true,
-                greater_than_or_equal_to: 0,
-                message: 'must be an integer greater than or equal to zero'
-              },
-              on: :create
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :likes_counter,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+              message: 'must be an integer greater than or equal to zero'
+            },
+            on: :create
+  validates :comments_counter,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0,
+              message: 'must be an integer greater than or equal to zero'
+            },
+            on: :create
 
-      def recent_comments(limit = 5)
+  def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
 
