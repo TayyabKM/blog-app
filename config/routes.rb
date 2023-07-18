a9086c7ecd6ad919a64263655d44f22042e7e0eb
Tayyab-Kamboh <tayyabkambohSK28@gmail.com>
2023-07-19 00:00:10 +0200

    Make required changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
-
-  get '/users/:id/posts', to: 'users#posts', as: 'posts_by_user'
-  get '/users/:user_id/posts/:id', to: 'users#post_show', as: 'post_by_user'
 end
