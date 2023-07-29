208128222da75d79d5a07f6e088f593983085592
TayyabKM <tayyabirfan05@gmail.com>
2023-07-29 02:49:58 +0200

    Add devise

--- db/schema.rb
+++ db/schema.rb
@@ -50,6 +50,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_26_200556) do
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
